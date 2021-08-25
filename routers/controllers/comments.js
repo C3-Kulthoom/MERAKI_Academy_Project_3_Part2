b718f42678738c4580e76dba20127cff36dccbc3
G11_Kulthoom <kulthumradi94@gmail.com>
2021-08-25 15:42:28 +0300

    last card try2

--- routers/controllers/comments.js
+++ routers/controllers/comments.js
@@ -1,27 +1,29 @@
 const Comment = require("../../db/models/commentsschema");
 const commentsModel = require("../../db/models/commentsschema");
+const createNewComment= (req,res)=>{
+const comment = req.body.comment
+const commenter = req.body.commenter
+const newComment = new commentModel({
+    comment,
+    commenter
+})
 
-const createNewComment =(req, res)=>{
-  const comment = req.body.comment 
-  const commenter =req.body.commenter
 
- 
-    const Comment = new commentsModel({
-        comment,
-        commenter
-    });
-  
-    Comment
-      .save()
-      .then((result) => {
-        res.status(201).json({success:true , massage:"the new comment added " , commenter : result });
-      })
-      .catch((err) => {
-        res.status(500).json({success:false  , massage:"server error " });
+newComment.save().then((result) => {
+   console.log(result)
+    res
+      .status(201)
+      .json({
+        success: true,
+        massage: "the new comment added",
+        commenter : result
       });
-  };
+  })
+  .catch((error) => {
+    res.status(500).json({ success: false, massage: "server error" });
+  });
 
 
-
+}
 
   module.exports = createNewComment 
